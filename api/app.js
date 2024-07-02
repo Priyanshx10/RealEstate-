2c4477a7c39ef9f94bb5fb61da6cd08eb9714093
Priyansh <Priyanshyadav1012@gmail.com>
2024-07-02 13:17:38 +0530

    Understanding Express Auth and installed Bcrypt for hashing the password

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import postRoute from './routes/post.route.js'; 
+import authRoute from './routes/auth.route.js';
 
 const app = express();
+app.use(express.json())
 
 // Mount the post route handler
 app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 // Start the server
 app.listen(8800, () => {
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,19 +1,11 @@
 import express from "express";
+import { login, logout, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/register",(req,res) => {
-    console.log("router Works")
-})
-
-router.post("/login",(req,res) => {
-    console.log("router Works")
-})
-
-
-router.post("/logout",(req,res) => {
-    console.log("router Works")
-})
+router.post("/register",register)
+router.post("/login",login)
+router.post("/logout", logout)
 
 
 export default router
